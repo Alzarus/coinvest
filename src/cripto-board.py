0c4bebc2b0e56efee9e5f3a9ff6cd1413a809b3f
Pedro Batista de Almeida Filho <pedro.filho@hbsis.com.br>
2022-01-21 08:48:34 -0300

    add sidebar update 21-02-22

--- src/cripto-board.py
+++ src/cripto-board.py
@@ -2,28 +2,26 @@
 # https://docs.streamlit.io/library/api-reference
 
 import json
+from turtle import title
 import numpy as np
 import pandas as pd
 import requests
 import streamlit as st
 import time
 
-st.title("Dashboard - criptomoedas por CoinMarketCap\nhttps://coinmarketcap.com/ ")
+# st.title("Dashboard - criptomoedas por CoinMarketCap\nhttps://coinmarketcap.com/ ")
+st.title("Investcrip - Dashboard para o seu investimento em Criptomoedas\n\nFonte dos dados: https://coinmarketcap.com/ ")
 
 BASE_URL = "https://pro-api.coinmarketcap.com"
 URL = BASE_URL + "/v1/cryptocurrency/listings/latest"
 # URL = BASE_URL + "/v1/global-metrics/quotes/latest"
-INVESTIMENT_TERM = st.radio(
-    "Prazo de investimento escolhido:", ('Curto prazo', 'Médio prazo', 'Longo prazo'))
-if INVESTIMENT_TERM:
-    st.write(f"Escolheu o {INVESTIMENT_TERM}")
-
-CRYPTO_LIMIT = st.slider(
-    "Quantidade de criptomoedas a serem exibidas:", min_value=1, max_value=50, value=5)
+INVESTIMENT_TERM = ""
+CRYPTO_LIMIT = ""
+CONVERT_CURRENCY = ""
 PARAMETERS = {
     'start': '1',
-    'limit': str(CRYPTO_LIMIT),
-    'convert': 'BRL'
+    'limit': "",
+    'convert': ""
 }
 HEADERS = {
     'Accepts': 'application/json',
@@ -31,11 +29,61 @@ HEADERS = {
 }
 
 
+# @st.cache
 def load_data():
+    global HEADERS, PARAMETERS
+
     response = requests.get(URL, headers=HEADERS, params=PARAMETERS)
     json_response = response.json()
+    total_coins = json_response["status"]["total_count"]
     # print(json.dumps(json_response, indent=4))
-    return json_response
+    json_response_data = json_response["data"]
+    for data in json_response_data:
+        data.pop("tags", None)
+    return json_response_data
 
 
-st.json(load_data())
+def load_sidebar():
+    global INVESTIMENT_TERM, CRYPTO_LIMIT, CONVERT_CURRENCY, PARAMETERS
+
+    st.sidebar.title("Opções")
+
+    INVESTIMENT_TERM = st.sidebar.radio(
+        "Prazo de investimento escolhido:", ('Curto prazo', 'Médio prazo', 'Longo prazo'))
+    # if INVESTIMENT_TERM:
+    #     st.sidebar.write(f"Escolheu o {INVESTIMENT_TERM}")
+
+    CRYPTO_LIMIT = st.sidebar.slider(
+        "Quantidade de criptomoedas a serem exibidas:", min_value=1, max_value=50, value=5)
+
+    CONVERT_CURRENCY = st.sidebar.selectbox(
+        "Moeda de conversão escolhida:", ("BRL", "USD"))
+
+    PARAMETERS = {
+        'start': '1',
+        'limit': str(CRYPTO_LIMIT),
+        'convert': f"{CONVERT_CURRENCY}"
+    }
+
+    st.sidebar.title("Sobre")
+    st.sidebar.info("Este dashboard é fruto do projeto de TCC do estudante de Análise"
+                    " e Desenvolvimento de Sistemas pelo IFBA - Campus Salvador:\n\n"
+                    "Pedro Batista de Almeida Filho")
+
+
+def main():
+    load_sidebar()
+    # TODO: CRIAR DATA FRAME COM TODOS OS OBJETOS (MOEDAS) DA LISTA, APENAS PEGANDO OS DADOS QUE TENHO INTERESSE
+    # TODO: CRIAR METODO ASYNC PARA ATUALIZAR O BOARD AUTOMATICAMENTE DPS DE X SEGUNDOS/MINUTOS
+    # TODO: REQUESTS -> BANCO DE DADOS -> MS DO DASH BUSCA NO BANCO DE DADOS (EVITAR A QUANTIDADE DE REQUESTS ELEVADA)
+    # https://github.com/paduel/streamlit_finance_chart/blob/master/app.py
+    # components = pd.DataFrame({
+    #     'first column': [1, 2, 3, 4],
+    #     'second column': [10, 20, 30, 40]
+    # })
+    # st.dataframe(components)
+    st.json(load_data())
+
+
+if __name__ == '__main__':
+    main()
